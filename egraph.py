2fb10be7d6086deb1b0bd8e146981fd8d5872ded
Chen Yu <chenyu.nus@gmail.com>
2022-12-28 16:21:23 +0800

    fix visualization

--- egraph.py
+++ egraph.py
@@ -8,7 +8,7 @@ from mini_lisp.core import RawLeaves, Symbols
 from mini_lisp.core_types import AstNode, Number, AstParent, Variable, Symbol
 from mini_lisp.patterns import MatchResult
 from mini_lisp.rules import Rule, RuleMatchResult, OPs
-from graph_visualization import MermaidGraph, NodeStyle
+from graph_visualization import MermaidGraph, NodeStyle, Linkable, Link, LinkableType
 from utils.misc import get_rounded_num
 
 AstP = AstNode[RawLeaves]
@@ -34,9 +34,9 @@ class EGraph:
             return self.classes[self.root_class].copy()
 
     def attach_ast_node_(self, ast: AstP) -> None:
-        # TODO this is not efficient
-        new_class_id = max(self.classes.keys(), default=0) + 1
         if ast not in self.registry:
+            # TODO this is not efficient
+            new_class_id = max(self.classes.keys(), default=0) + 1
             self.registry[ast] = new_class_id
             self.classes[new_class_id] = {ast}
 
@@ -46,7 +46,7 @@ class EGraph:
             egraph.attach_ast_node_(ast)
         else:
             if not isinstance(ast, AstParent):
-                raise Exception(f"Ast {ast} has unknown type: {type(ast)}")
+                raise Exception(f"Ast {ast} has unexpected type: {type(ast)}")
             for arg in ast.args:
                 EGraph.attach_ast_(arg, egraph)
             egraph.attach_ast_node_(ast)
@@ -140,10 +140,6 @@ class EGraph:
         print(self.registry)
         ## assume the dict is in order (python >= 3.6)
         node_ids: Dict[AstP, int] = {n: i for i, n in enumerate(self.registry.keys()) if n not in OPs}
-        node_links = list((node_from_id, node_ids[node_to])
-                          for node_from, node_from_id in node_ids.items()
-                          if isinstance(node_from, AstParent)
-                          for node_to in node_from.args[1:])
 
         style_gen = NodeStyle.get_style_gen()
 
@@ -157,50 +153,28 @@ class EGraph:
                     raise RuntimeError("No OPs allowed here")
             return style_gen(4)
 
-        subgraph_id, subgraph_content = list(zip(*filter(lambda x: len(x[-1]) > 0,
-                                                         ((i, frozenset(node_ids[n] for n in v if n not in OPs))
-                                                          for i, v in self.classes.items()))))
+        subgraph_eclass_id, subgraph_content = list(zip(*filter(lambda x: len(x[-1]) > 0,
+                                                                ((i, frozenset(node_ids[n] for n in v if n not in OPs))
+                                                                 for i, v in self.classes.items()))))
+        subgraph_eclass_id_to_idx = {v: i for i, v in enumerate(subgraph_eclass_id)}
+
+        def make_link(i: int, j: int, arg_n: int) -> Link:
+            return Link(Linkable(i, LinkableType.Node), Linkable(j, LinkableType.Subgraph), content=f"{arg_n}")
+
+        links = list(make_link(node_from_id, subgraph_eclass_id_to_idx[self.registry[node_to]], arg_n)
+                     for node_from, node_from_id in node_ids.items()
+                     if isinstance(node_from, AstParent)
+                     for arg_n, node_to in enumerate(node_from.args[1:]))
+
         return MermaidGraph(
             sub_graphs=subgraph_content,
-            links=[(i, j) for i, j in node_links],
-            sub_graph_links=[],
-            subgraph_names=[f"\"{get_rounded_num(i)}\"" for i in subgraph_id],
-            node_names={node_ids[k]: f"\"{k.display}\""
+            links=links,
+            subgraph_names=[f"\"{get_rounded_num(i)}\"" for i in subgraph_eclass_id],
+            node_names={node_ids[k]: f"{k.display}"
             if not isinstance(k, AstParent)
-            else f"\"{k.args[0].display}\""
+            else f"{k.args[0].display}"
                         for k in self.registry.keys()
                         if k not in OPs},
             node_styles={node_ids[k]: style_format(k) for k in self.registry.keys() if k not in OPs},
         )
 
-    def to_mermaid_org(self) -> MermaidGraph:
-        ## assume the dict is in order (python >= 3.6)
-        node_ids: Dict[AstP, int] = {n: i for i, n in enumerate(self.registry.keys())}
-        node_links = list({(node_from_id, node_ids[node_to])
-                           for node_from, node_from_id in node_ids.items()
-                           if isinstance(node_from, AstParent)
-                           for node_to in node_from.args})
-
-        style_gen = NodeStyle.get_style_gen()
-
-        def style_format(node: AstNode[RawLeaves]) -> NodeStyle:
-            if isinstance(node, AstParent):
-                return style_gen(0)
-            elif isinstance(node, Number):
-                return style_gen(2)
-            elif isinstance(node, Variable):
-                if node in OPs:
-                    return style_gen(1)
-            return style_gen(4)
-
-        return MermaidGraph(
-            sub_graphs=[frozenset(node_ids[i] for i in s) for s in self.classes.values()],
-            links=[(i, j) for i, j in node_links],
-            sub_graph_links=[],
-            subgraph_names=[f"\"{get_rounded_num(k)}\"" for k in self.classes.keys()],
-            node_names={node_ids[k]: f"\"{k.display}\""
-            if not isinstance(k, AstParent)
-            else f"\"eval({MermaidGraph.node_fmt(node_ids[k])})\""
-                        for k in self.registry.keys()},
-            node_styles={node_ids[k]: style_format(k) for k in self.registry.keys()},
-        )
